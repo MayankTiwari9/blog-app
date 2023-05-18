2bbfb6d0883d7298e7812d81ec4e358ee6935370
Mayank tiwari <mt1027319@gmail.com>
2023-05-18 16:00:17 +0530

    Completed

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ dotenv.config({path: "./config/config.env"})
 
 connectDataBase();
 
-app.listen(process.env.PORT, () => {
+
+
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
 })
 
+process.on("unhandledRejection", (err) => {
+    console.log(`Error; ${err.message}`);
+    console.log('Shutting down the server due to Unhandled Promise Rejection');
+
+    server.close(() => {
+        process.exit(1);
+    })
+})
+
+process.on("uncaughtException", (err) => {
+    console.log(`Error; ${err.message}`);
+    console.log('Shutting down the server due to Unhandled Uncaught Exception');
+
+        process.exit(1);  
+})
